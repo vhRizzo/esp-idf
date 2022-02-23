6fec3d782d4a936ce5ff59ed14734927112b8fa7
vhRizzo <43479155+vhRizzo@users.noreply.github.com>
2022-02-23 20:18:59 -0300

    Update global_data.cpp

--- inmp441_test/main/global_data.cpp
+++ inmp441_test/main/global_data.cpp
@@ -5,7 +5,9 @@ void print_task ( void *pvParameters )
     inmp441_t inmp;
     while (true) {
         if (xQueueReceive(inmp_queue, &inmp, portMAX_DELAY)) {
-            ESP_LOGI(__func__, "%.2f", inmp.ruido);
+            xTaskNotifyGive(xTaskGetCurrentTaskHandle());
         }
+        if (ulTaskNotifyTakeIndexed(0, pdTRUE, portMAX_DELAY) == pdTRUE)
+            ESP_LOGI(__func__, "%.2f", inmp.ruido);
     }
 }
