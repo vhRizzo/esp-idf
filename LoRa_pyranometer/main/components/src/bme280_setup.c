d5367b5c4586b155adeb5817c20198c7a0b90f20
Victor Hugo Rizzo Moura <43479155+vhRizzo@users.noreply.github.com>
2023-02-24 17:47:36 -0300

    documented changes from source code

--- LoRa_pyranometer/main/components/src/bme280_setup.c
+++ LoRa_pyranometer/main/components/src/bme280_setup.c
@@ -1,18 +1,22 @@
 #include "global_data.h"
+/* All libraries used throughout the entire project were moved to global_data header. */
 
 #ifdef BME280_SENSOR
 #include "bme280_setup.h"
 
+/* Defines moved to other headers and/or removed */
 struct bme280_data comp_data;
 struct bme280_dev bme;
 uint8_t settings_sel;
 int8_t bmestatus = BME280_OK;
-bme280_t bme_dados;
-QueueHandle_t bme_queue;
+bme280_t bme_dados;     // Item to store sensor's data
+QueueHandle_t bme_queue;// Handler for the queue to send above item
 
+/* All original comments was either removed or translated to portuguese.
+ * All printf() calls was changed to ESP_LOG pattern. */
 void i2c_master_init()
 {
-    i2c_config_t i2c_config;
+    i2c_config_t i2c_config;    // Changed how i2c_config is initialized to get rid of compiler warnings about uninitialized values
     i2c_config.mode = I2C_MODE_MASTER;
     i2c_config.sda_io_num = BME_SDA_PIN;
     i2c_config.scl_io_num = BME_SCL_PIN;
@@ -21,7 +25,7 @@ void i2c_master_init()
     i2c_config.master.clk_speed = 100000;
     i2c_param_config(I2C_NUM_0, &i2c_config);
     i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
- 
+    
 //*************// Verifica se o slave I2C esta funcionando corretamente.
     esp_err_t f_retval;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
@@ -85,6 +89,7 @@ int8_t i2c_reg_write(uint8_t reg_addr, uint8_t *reg_data, uint32_t length, void 
     return iError;
 }
 
+/* Changed to microseconds to match Bosch's pattern, and to freeRTOS' delay method. */
 void delay_us(uint32_t us, void *intf_ptr) { vTaskDelay((us/1000) / portTICK_PERIOD_MS); }
 
 void print_rslt(const char api_name[], int8_t rslt)
@@ -109,6 +114,7 @@ void print_rslt(const char api_name[], int8_t rslt)
     }
 }
 
+/* Changed to send data through the queue intead of printing it */
 void store_data (struct bme280_data *comp_data)
 {
     bme_dados.temperatura = comp_data->temperature; 
@@ -117,8 +123,10 @@ void store_data (struct bme280_data *comp_data)
     xQueueOverwrite(bme_queue, &bme_dados);
 }
 
+/* Only forced mode will be used, so normal mode was removed, and the function was changed to freeRTOS task's pattern. */
 void bme280_task(void *pvParameters)
 {
+    /* Stuff from app_main was moved here to be called before the loop. */
     i2c_master_init();
 
     uint8_t dev_addr = BME280_I2C_ADDR_PRIM;    // 0x76
@@ -145,6 +153,7 @@ void bme280_task(void *pvParameters)
     print_rslt("bme280_set_sensor_settings status", bmestatus);
     #endif
     while (1) {
+        /* Don't print sensor status, and just delay normally instead of using Bosch's method. */
         bmestatus = bme280_set_sensor_mode(BME280_FORCED_MODE, &bme);
         vTaskDelay( (TEMPO_ANALISE*1000)/portTICK_PERIOD_MS );
         bmestatus = bme280_get_sensor_data(BME280_ALL, &comp_data, &bme);
