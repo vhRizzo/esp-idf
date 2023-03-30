e4fdd3076a694241688b28b2fed2e0eb845bb43c
Victor Hugo Rizzo Moura <43479155+vhRizzo@users.noreply.github.com>
2023-03-30 09:05:02 -0300

    General update

--- LoRa_pyranometer/main/components/include/global_data.h
+++ LoRa_pyranometer/main/components/include/global_data.h
@@ -2,53 +2,48 @@
 #define GLOBAL_DATA_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif /* C++ Guard */
 
 /* Geral */
-#define TEMPO_ANALISE   30  // segundos
-#define TEMPO_STARTUP   5   // segundos
-#define CONTAGEM_GPS    3  // a cada quantas coletas de dados serão enviadas as coordenadas GPS.
-    
-/* Comentar/remover os defines ira desabilitar os respectivos processos */
+#define TEMPO_ANALISE 30 // segundos
+#define TEMPO_STARTUP 5  // segundos
+#define CONTAGEM_GPS 3   // a cada quantas coletas de dados serão enviadas as coordenadas GPS.
+
+/* Comentar/remover os defines irá desabilitar os respectivos processos */
 #define DSM501A_SENSOR
 #define BME280_SENSOR
 #define NEO6M_SENSOR
-#define DS18B20_SENSOR
-#define BPW34_SENSOR
+// #define DS18B20_SENSOR
+#define ADS1115_SENSOR
 #define SERIAL_DEBUG
 
 /* Pinout */
-#define BME_SDA_PIN     GPIO_NUM_21 // (D21)
-#define BME_SCL_PIN     GPIO_NUM_22 // (D22)
-#define DSM_P10_PIN     GPIO_NUM_34 // (D34)
-#define DSM_P25_PIN     GPIO_NUM_35 // (D35)
-#define NEO_UTX_PIN     GPIO_NUM_32 // (D32)
-#define NEO_URX_PIN     GPIO_NUM_33 // (D33)
-#define LORA_TX_PIN     GPIO_NUM_12 // (D12)
-#define LORA_RX_PIN     GPIO_NUM_13 // (D13)
-#define DS18B20_PIN     GPIO_NUM_27 // (D27)
-#define BPW_ADC_PIN     GPIO_NUM_36 // (VP)
-/*  
- *  O ESP32 tem 2 unidades de ADC, a unidade 2 possui algumas limitacoes, portanto e preferivel
- *  a utilizacao da unidade 1.
- *  A unidade 1 possui 7 canais disponiveis, presentes nos pinos 32 a 39.
- */
+#define I2C_SDA_PIN GPIO_NUM_21 // (D21)
+#define I2C_SCL_PIN GPIO_NUM_22 // (D22)
+#define DSM_P10_PIN GPIO_NUM_34 // (D34)
+#define DSM_P25_PIN GPIO_NUM_35 // (D35)
+#define NEO_UTX_PIN GPIO_NUM_32 // (D32)
+#define NEO_URX_PIN GPIO_NUM_33 // (D33)
+#define LORA_TX_PIN GPIO_NUM_12 // (D12)
+#define LORA_RX_PIN GPIO_NUM_13 // (D13)
+#define DS18B20_PIN GPIO_NUM_27 // (D27)
 
 /* Portas UART */
-#define NEO_UART_PORT   UART_NUM_1
-#define LOR_UART_PORT   UART_NUM_2
+#define NEO_UART_PORT UART_NUM_1
+#define LOR_UART_PORT UART_NUM_2
+
+/* Porta I2C */
+#define I2C_PORT I2C_NUM_0
 
 #include <math.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include "esp_adc/adc_cali_scheme.h"
-#include "esp_adc/adc_oneshot.h"
 #include "esp32/rom/ets_sys.h"
 #include "freertos/FreeRTOS.h"
-#include "esp_adc/adc_cali.h"
 #include "freertos/queue.h"
 #include "freertos/task.h"
 #include "soc/soc_caps.h"
@@ -63,69 +58,72 @@ extern "C" {
 #include "esp_log.h"
 #include "esp_now.h"
 
-void rcv_data_task ( void *pvParameters );
-void snd_data_task ( void *pvParameters );
+#if defined(BME280_SENSOR) || defined(ADS1115_SENSOR)
+    esp_err_t i2c_init();
+#endif
+    void rcv_data_task(void *pvParameters);
+    void snd_data_task(void *pvParameters);
 
 #ifdef DSM501A_SENSOR
-typedef struct __attribute__((__packed__))  // Cria uma struct/item com as variaveis necessarias para armazenar os dados de seu respectivo sensor
-{
-    float poeira_pm_10;
-    float poeira_pm_25;
-} dsm501a_t;
+    typedef struct __attribute__((__packed__)) // Cria uma struct/item com as variaveis necessarias para armazenar os dados de seu respectivo sensor
+    {
+        float poeira_pm_10;
+        float poeira_pm_25;
+    } dsm501a_t;
 
-extern QueueHandle_t dsm_queue;             // Cria uma handler para a fila de recebimento de itens, marcado como "extern" para que o compilador saiba
-                                            // que ela sera inicializada por uma fonte externa.
+    extern QueueHandle_t dsm_queue; // Cria um handler para a fila de recebimento de itens, marcado como "extern" para que o compilador saiba
+                                    // que ela será inicializada por uma fonte externa.
 #endif
 
 #ifdef BME280_SENSOR
-typedef struct __attribute__((__packed__))
-{
-    float temperatura;
-    float umidade;
-    uint32_t pressao;
-} bme280_t;
+    typedef struct __attribute__((__packed__))
+    {
+        float temperatura;
+        float umidade;
+        uint32_t pressao;
+    } bme280_t;
 
-extern QueueHandle_t bme_queue;
+    extern QueueHandle_t bme_queue;
 #endif
 
 #ifdef NEO6M_SENSOR
-typedef struct __attribute__((__packed__))
-{
-    float coord[2]; //[0] Lat , [1] Lng
-} neo6m_t;
+    typedef struct __attribute__((__packed__))
+    {
+        float coord[2]; //[0] Lat , [1] Lng
+    } neo6m_t;
 
-extern QueueHandle_t neo_queue;
+    extern QueueHandle_t neo_queue;
 #endif
 
 #ifdef DS18B20_SENSOR
-typedef struct __attribute__((__packed__))
-{
-    float dallas_temp;
-} ds18b20_t;
+    typedef struct __attribute__((__packed__))
+    {
+        float dallas_temp;
+    } ds18b20_t;
 
-extern QueueHandle_t ds18b20_queue;
+    extern QueueHandle_t ds18b20_queue;
 #endif
 
-#ifdef BPW34_SENSOR
-typedef struct __attribute__((__packed__))
-{
-    float irrad;
-} bpw34_t;
+#ifdef ADS1115_SENSOR
+    typedef struct __attribute__((__packed__))
+    {
+        float irrad;
+    } ads_t;
 
-extern QueueHandle_t bpw34_queue;
+    extern QueueHandle_t ads_queue;
 #endif
 
-typedef struct __attribute__((__packed__)) //esse atributo informa ao compilador para utilizar o mínimo de memória para tipos dentro da struct ou da union
-{
-    float temperatura;      // 4 bytes
-    float umidade;          // 4 bytes
-    uint32_t pressao;       // 2 bytes
-    float dallas_temp;      // 4 bytes
-    float irrad;            // 4 bytes
-    float poeira_pm_10;     // 4 bytes
-    float poeira_pm_25;     // 4 bytes
-    float coord[2];         // 8 bytes
-} dados_t;
+    typedef struct __attribute__((__packed__)) // esse atributo informa ao compilador para utilizar o mínimo de memória para tipos dentro da struct ou da union
+    {
+        float temperatura;  // 4 bytes
+        float umidade;      // 4 bytes
+        uint32_t pressao;   // 2 bytes
+        float dallas_temp;  // 4 bytes
+        float irrad;        // 4 bytes
+        float poeira_pm_10; // 4 bytes
+        float poeira_pm_25; // 4 bytes
+        float coord[2];     // 8 bytes
+    } dados_t;
 
 #ifdef __cplusplus
 }
--- LoRa_pyranometer/main/components/src/global_data.cpp
+++ LoRa_pyranometer/main/components/src/global_data.cpp
@@ -10,21 +10,20 @@ dados_t dados = {
     .irrad = -1,
     .poeira_pm_10 = -1,
     .poeira_pm_25 = -1,
-    .coord = {-1,-1}
-};
-char tmp[100];              // String temporaria para formatar os dados
-char rcv[1000];             // String para armazenar a resposta da LoRaWAN
-char rcv_gps[1000];         // Outra para a resposta do GPS
-uint16_t porta = 20;        // Aqui deve-se escolher >=1 , sendo útil para indicar qual parte da aplicação pertence o dado obtido!!!!
-uint32_t rcv_size = 0;      // Variavel para armazenar o tamanho da resposta
-uint32_t send_size = 0;     // E o tamanho dos dados enviados
-uint16_t porta_gps = 19;    // Porta utilizada para as coordenadas GPS
-uint16_t porta_sol = 18;    // Porta utilizada para os sensores relacionados ao piranômetro
-uint32_t rcv_gps_size = 0;  // Variavel para armazenar o tamanho da resposta ao envio das coordenadas GPS
-Modem_SmartModular teste(LOR_UART_PORT);    // Inicializa a classe da LoRaWAN utilizando um construtor com a porta UART a ser utilizada pelo ESP
-int cont_gps = (int)floor(CONTAGEM_GPS*(2./3.));    // Inicializa o contador de envio de coordenadas GPS em 2/3 do valor total, desta forma as coordenadas
-                                                    // nao serao enviadas imediatamente no inicio (pois o modulo GPS provavelmente nao tera sincronizado
-                                                    // com o satelite ainda), mas tambem nao espera um ciclo completo para enviar a primeira medida.
+    .coord = {-1, -1}};
+char tmp[100];                                       // String temporaria para formatar os dados
+char rcv[1000];                                      // String para armazenar a resposta da LoRaWAN
+char rcv_gps[1000];                                  // Outra para a resposta do GPS
+uint16_t porta = 20;                                 // Aqui deve-se escolher >=1 , sendo útil para indicar qual parte da aplicação pertence o dado obtido!!!!
+uint32_t rcv_size = 0;                               // Variavel para armazenar o tamanho da resposta
+uint32_t send_size = 0;                              // E o tamanho dos dados enviados
+uint16_t porta_gps = 19;                             // Porta utilizada para as coordenadas GPS
+uint16_t porta_sol = 18;                             // Porta utilizada para os sensores relacionados ao piranômetro
+uint32_t rcv_gps_size = 0;                           // Variavel para armazenar o tamanho da resposta ao envio das coordenadas GPS
+Modem_SmartModular teste(LOR_UART_PORT);             // Inicializa a classe da LoRaWAN utilizando um construtor com a porta UART a ser utilizada pelo ESP
+int cont_gps = (int)floor(CONTAGEM_GPS * (2. / 3.)); // Inicializa o contador de envio de coordenadas GPS em 2/3 do valor total, desta forma as coordenadas
+                                                     // nao serao enviadas imediatamente no inicio (pois o modulo GPS provavelmente nao tera sincronizado
+                                                     // com o satelite ainda), mas tambem nao espera um ciclo completo para enviar a primeira medida.
 /* Inicializa os itens que serao recebidos atraves das filas */
 #ifdef DSM501A_SENSOR
 dsm501a_t dsm_rcv;
@@ -41,30 +40,48 @@ ds18b20_t dallas_rcv;
 #ifdef BPW34_SENSOR
 bpw34_t bpw_rcv;
 #endif
+#ifdef ADS1115_SENSOR
+ads_t ads_rcv;
+#endif
+
+#if defined(BME280_SENSOR) || defined(ADS1115_SENSOR)
+esp_err_t i2c_init()
+{
+    i2c_config_t i2c_config; // Changed how i2c_config is initialized to get rid of compiler warnings about uninitialized values
+    i2c_config.mode = I2C_MODE_MASTER;
+    i2c_config.sda_io_num = I2C_SDA_PIN;
+    i2c_config.scl_io_num = I2C_SCL_PIN;
+    i2c_config.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    i2c_config.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    i2c_config.master.clk_speed = 100000;
+    i2c_param_config(I2C_PORT, &i2c_config);
+    return i2c_driver_install(I2C_PORT, I2C_MODE_MASTER, 0, 0, 0);
+}
+#endif
 
 /* Esta tarefa apenas se mantem em um loop recebendo os itens armazenados nas filas */
-void rcv_data_task ( void *pvParameters )
+void rcv_data_task(void *pvParameters)
 {
     while (1)
     {
-        #ifdef DSM501A_SENSOR
-        if (xQueueReceive(dsm_queue, &dsm_rcv, portMAX_DELAY))  // Ao receber um novo item pela fila...
+#ifdef DSM501A_SENSOR
+        if (xQueueReceive(dsm_queue, &dsm_rcv, portMAX_DELAY)) // Ao receber um novo item pela fila...
         {
-            dados.poeira_pm_10 = dsm_rcv.poeira_pm_10;          // Armazena os dados do item recebido no item de dados gerais
+            dados.poeira_pm_10 = dsm_rcv.poeira_pm_10; // Armazena os dados do item recebido no item de dados gerais
             dados.poeira_pm_25 = dsm_rcv.poeira_pm_25;
         }
-        #endif
+#endif
 
-        #ifdef BME280_SENSOR
+#ifdef BME280_SENSOR
         if (xQueueReceive(bme_queue, &bme_rcv, portMAX_DELAY))
         {
             dados.temperatura = bme_rcv.temperatura;
             dados.umidade = bme_rcv.umidade;
             dados.pressao = bme_rcv.pressao;
         }
-        #endif
+#endif
 
-        #ifdef NEO6M_SENSOR
+#ifdef NEO6M_SENSOR
         if (xQueueReceive(neo_queue, &neo_rcv, portMAX_DELAY))
         {
             /* Por vezes, ao tentar ler os valores do GPS antes do modulo ter sincronizado com o satelite, ele enviara valores de lixo.
@@ -77,89 +94,99 @@ void rcv_data_task ( void *pvParameters )
             if (fabs(neo_rcv.coord[1]) > 0 && fabs(neo_rcv.coord[1]) <= 180)
                 dados.coord[1] = neo_rcv.coord[1];
         }
-        #endif
+#endif
 
-        #ifdef DS18B20_SENSOR
+#ifdef DS18B20_SENSOR
         if (xQueueReceive(ds18b20_queue, &dallas_rcv, portMAX_DELAY))
         {
             dados.dallas_temp = dallas_rcv.dallas_temp;
         }
-        #endif
-        
-        #ifdef BPW34_SENSOR
+#endif
+
+#ifdef BPW34_SENSOR
         if (xQueueReceive(bpw34_queue, &bpw_rcv, portMAX_DELAY))
         {
             dados.irrad = bpw_rcv.irrad;
         }
-        #endif
+#endif
+
+#ifdef ADS1115_SENSOR
+        if (xQueueReceive(ads_queue, &ads_rcv, portMAX_DELAY))
+        {
+            dados.irrad = ads_rcv.irrad;
+        }
+#endif
     }
 }
 
 /* Esta tarefa inicializa o modulo LoRa, e envia os dados recebidos para uma antena LoRa */
-void snd_data_task ( void *pvParameters )
+void snd_data_task(void *pvParameters)
 {
-    memset(rcv,0,1000);     // Inicializa a string de resposta dos dados gerais da LoRa
-    memset(rcv_gps,0,1000); // E a de dados do gps
-    
-    teste.OTAA_DEUI();      // Exibe o endereco MAC do modulo
-    #ifdef SERIAL_DEBUG
-    ESP_LOGI(__func__, "%s", teste.OTAA_APPKEY());  // Exibe a chave de validacao do modulo
-    #endif
+    memset(rcv, 0, 1000);     // Inicializa a string de resposta dos dados gerais da LoRa
+    memset(rcv_gps, 0, 1000); // E a de dados do gps
 
-    if(strcmp(teste.auto_join(),"0")==0){   // Verifica se o autojoin esta ativado, se nao estiver, o ativa
-        #ifdef SERIAL_DEBUG
+    teste.OTAA_DEUI(); // Exibe o endereco MAC do modulo
+#ifdef SERIAL_DEBUG
+    ESP_LOGI(__func__, "%s", teste.OTAA_APPKEY()); // Exibe a chave de validacao do modulo
+#endif
+
+    if (strcmp(teste.auto_join(), "0") == 0)
+    { // Verifica se o autojoin esta ativado, se nao estiver, o ativa
+#ifdef SERIAL_DEBUG
         ESP_LOGI("APP", "AutoJoin ativado: %s", teste.auto_join(1));
-        #endif
+#endif
     }
-    else{
-        #ifdef SERIAL_DEBUG
+    else
+    {
+#ifdef SERIAL_DEBUG
         ESP_LOGI("APP", "AutoJoin já ativado...");
-        #endif
+#endif
     }
 
-    vTaskDelay( (TEMPO_STARTUP*1000)/portTICK_PERIOD_MS ); // Delay necessario para que alguns sensores estejam em seu estado estavel
-    while(1)
+    vTaskDelay((TEMPO_STARTUP * 1000) / portTICK_PERIOD_MS); // Delay necessario para que alguns sensores estejam em seu estado estavel
+    while (1)
     {
-        vTaskDelay( (TEMPO_ANALISE*1000)/portTICK_PERIOD_MS );  // Delay para cada leitura
-        char tmp[100];                                          // String auxiliar para formatar os dados que serao enviados para a antena LoRa
+        vTaskDelay((TEMPO_ANALISE * 1000) / portTICK_PERIOD_MS); // Delay para cada leitura
+        char tmp[100];                                           // String auxiliar para formatar os dados que serao enviados para a antena LoRa
         sprintf(tmp, "%.2f,%.2f,%d;%.2f,%.2f",
                 dados.temperatura, dados.umidade, (int)dados.pressao,
-                dados.poeira_pm_10, dados.poeira_pm_25);        // String que contera as informacoes relacionadas ao ambiente
-        send_size = strlen(tmp);                                // Armazena o tamanho da string
-        #ifdef SERIAL_DEBUG
+                dados.poeira_pm_10, dados.poeira_pm_25); // String que contera as informacoes relacionadas ao ambiente
+        send_size = strlen(tmp);                         // Armazena o tamanho da string
+#ifdef SERIAL_DEBUG
         ESP_LOGI(__func__, "ENVIOU = DADOS: %s | TAM: %i | PORTA: %d", tmp, (int)send_size, porta);
-        #endif
-        teste.enviar_receber(porta, tmp, send_size, rcv, &rcv_size);    // Envia os dados na porta dos dados ambientais
-        #ifdef SERIAL_DEBUG
+#endif
+        teste.enviar_receber(porta, tmp, send_size, rcv, &rcv_size); // Envia os dados na porta dos dados ambientais
+#ifdef SERIAL_DEBUG
         ESP_LOGI(__func__, "RECEBEU = DADOS: %s | TAM: %i", rcv, (int)rcv_size);
-        #endif
+#endif
 
-        sprintf(tmp, "%.2f,%.2f", 
-                dados.dallas_temp, dados.irrad);                // String que contera as informacoes relacionadas ao piranometro
-        send_size = strlen(tmp);                                // Armazena o tamanho da string
-        #ifdef SERIAL_DEBUG
+        sprintf(tmp, "%.2f,%.2f",
+                dados.dallas_temp, dados.irrad); // String que contera as informacoes relacionadas ao piranometro
+        send_size = strlen(tmp);                 // Armazena o tamanho da string
+#ifdef SERIAL_DEBUG
         ESP_LOGI(__func__, "ENVIOU = DADOS: %s | TAM: %i | PORTA: %d", tmp, (int)send_size, porta_sol);
-        #endif
-        teste.enviar_receber(porta_sol, tmp, send_size, rcv, &rcv_size);// Envia os dados na porta dos dados do piranometro
-        #ifdef SERIAL_DEBUG
+#endif
+        teste.enviar_receber(porta_sol, tmp, send_size, rcv, &rcv_size); // Envia os dados na porta dos dados do piranometro
+#ifdef SERIAL_DEBUG
         ESP_LOGI(__func__, "RECEBEU = DADOS: %s | TAM: %i", rcv, (int)rcv_size);
-        #endif
+#endif
 
-        #ifdef NEO6M_SENSOR
-        cont_gps++;                                         // Incrementa o contados do gps
-        if (cont_gps >= CONTAGEM_GPS) {                         // Se o contador for maior que a quantidade de contagem definida no header
-            char tmp_gps[100];                                  // Cria uma string propria para armazenar os dados do gps
-            sprintf(tmp_gps, "%.6f,%.6f", dados.coord[0], dados.coord[1]);  // Formata as coordenadas com precisao de 6 casas decimais
-            send_size = strlen(tmp_gps);                        // Armazena o tamanho da string
-            #ifdef SERIAL_DEBUG
+#ifdef NEO6M_SENSOR
+        cont_gps++; // Incrementa o contados do gps
+        if (cont_gps >= CONTAGEM_GPS)
+        {                                                                  // Se o contador for maior que a quantidade de contagem definida no header
+            char tmp_gps[100];                                             // Cria uma string propria para armazenar os dados do gps
+            sprintf(tmp_gps, "%.6f,%.6f", dados.coord[0], dados.coord[1]); // Formata as coordenadas com precisao de 6 casas decimais
+            send_size = strlen(tmp_gps);                                   // Armazena o tamanho da string
+#ifdef SERIAL_DEBUG
             ESP_LOGI(__func__, "ENVIOU = DADOS: %s | TAM: %i | PORTA: %d", tmp_gps, (int)send_size, porta_gps);
-            #endif
-            teste.enviar_receber(porta_gps, tmp_gps, send_size, rcv_gps, &rcv_gps_size);    // Envia os dados na porta relacionada ao gps
-            #ifdef SERIAL_DEBUG
+#endif
+            teste.enviar_receber(porta_gps, tmp_gps, send_size, rcv_gps, &rcv_gps_size); // Envia os dados na porta relacionada ao gps
+#ifdef SERIAL_DEBUG
             ESP_LOGI(__func__, "RECEBEU = DADOS: %s | TAM: %i", rcv_gps, (int)rcv_gps_size);
-            #endif
-            cont_gps = 0;                                       // Reinicia o contador do gps
+#endif
+            cont_gps = 0; // Reinicia o contador do gps
         }
-        #endif
+#endif
     }
 }
